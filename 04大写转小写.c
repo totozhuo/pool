01bb11643573fb9cca3d1397a90ca34185948d85
lizhuo <1713147195@qq.com>
2022-03-09 22:35:43 +0800

    李卓的修改

---
+++ 04大写转小写.c
@@ -0,0 +1,25 @@
+/***************************************************
+> Copyright (C) 2021 ==KINGYI== All rights reserved.
+> File Name: 04大写转小写.c
+> Author: Kingyi
+> Mail:joseph2009@163.com 
+> Created Time: 2021年11月09日 星期二 11时30分13秒
+***************************************************/
+
+#include <stdio.h>
+// w shi lizhuo
+char toLower(char ch)
+{
+	return ch-'A'+'a';
+}
+
+int main(int argc,const char* argv[])
+{
+	char ch = 0;
+
+	printf("请输入一个字母：\n");
+	scanf("%c",&ch);
+
+	printf("%c\n",toLower(ch));
+    return 0;
+}
